a73264a0a263659a530de0bdfcdff2f176a22371
purefootsoldier <58126102+purefootsoldier@users.noreply.github.com>
2024-10-16 15:06:51 -0500

    configured cors

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -14,6 +14,14 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<LoginResponse>
 ) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const {method} = req;
 
     if (method == 'POST'){
@@ -24,17 +32,18 @@ export default async function handler(
         const user = await prisma.user.findUnique({
             where: {username},
         });
+        console.log(user)
 
         if (!user){
             
             return res.status(401).json({error:'Usuario no encontrado'});
         }
 
-        //const isPasswordValid = await bcypt.compare(password, userBd.password);
+        const isPasswordValid = await bcypt.compare(password, user.password);
 
-        //if (!isPasswordValid){
-            //return res.status(401).json({error:'Contraseña incorrecta'});
-        //}
+        if (!isPasswordValid){
+            return res.status(401).json({error:'Contraseña incorrecta'});
+        }
 
         //Generamos el token JWT
         const token = jwt.sign(
