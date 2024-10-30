84c5bc4f17add39a106fad94d0503a7f28025a6a
Citlaly Bocanegra <cytla.bocanegra@gmail.com>
2024-10-29 22:03:40 -0500

    CambiosRealizados

--- pages/api/product.ts
+++ pages/api/product.ts
@@ -1,6 +1,7 @@
 import type {  NextApiRequest, NextApiResponse } from "next";
 import jwt from 'jsonwebtoken';
 import { PrismaClient } from "@prisma/client";
+import bcypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
@@ -13,6 +14,14 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<LoginResponse>
 ) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
     const {method} = req;
 
     if (method == 'POST'){
@@ -22,12 +31,18 @@ export default async function handler(
         const user = await prisma.user.findUnique({
             where: {username},
         });
+        console.log(user)
 
         if (!user){
             
             return res.status(401).json({error:'Usuario no encontrado'});
         }
 
+        const isPasswordValid = await bcypt.compare(password, user.password);
+
+        if (!isPasswordValid){
+            return res.status(401).json({error:'Contraseña incorrecta'});
+        }
 
         //Generamos el token JWT
         const token = jwt.sign(
---
+++ prisma/migrations/20241030000309_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `id_reserva` on the `clientes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `clientes` DROP COLUMN `id_reserva`;
