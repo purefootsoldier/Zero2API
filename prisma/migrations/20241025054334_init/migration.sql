2207aea29bd2918ddeed1bf802e8a2246ce078d3
FGArandaRodriguez <franklingiovanni97@gmail.com>
2024-11-05 11:40:22 -0500

    Correcion de migraciones y schema prisma

--- prisma/migrations/20241025054334_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `Pedidos` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `fecha` DATETIME(3) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20241030000309_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `id_reserva` on the `clientes` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `clientes` DROP COLUMN `id_reserva`;
